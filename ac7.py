285a6ad86617ee666b2f9202a0f97f6a3ea7fad6
jashvathspspjashvath-alt <jashvathspspjashvath@gmail.com>
2025-09-25 20:52:48 +0530

    Add files via upload

---
+++ ac7.py
@@ -0,0 +1,10 @@
+'''Raj scored 40, 70, 50 and 60 out of 100 in maths, science, Hindi and English.
+ Find the percentage he got?'''
+maths=40
+science=70
+hindi=50
+english=60
+summ=maths+science+hindi+english
+print(summ)
+percentage=(summ/400)*100
+print(percentage)
