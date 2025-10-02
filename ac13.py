5e6b4427e45f8f1167f9a0875892ea80e75d8919
jashvathspspjashvath-alt <jashvathspspjashvath@gmail.com>
2025-10-02 20:30:50 +0530

    Add files via upload

---
+++ ac13.py
@@ -0,0 +1,15 @@
+x=True
+y=False
+a=69
+if x==True or x==False:
+    print('yes it is a boolean value')
+else:
+    print('not a boolean value')
+if y==True or y==False:
+    print('yes it is a boolean value')
+else:
+    print('not a noolean value')
+if a==True or a== False:
+    print('yes it is a boolean value')
+else:
+    print('not a boolean value')
