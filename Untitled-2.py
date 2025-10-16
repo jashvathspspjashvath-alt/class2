bb1595186c6d8ab43d945d6b2426262907df3c5d
jashvathspspjashvath-alt <jashvathspspjashvath@gmail.com>
2025-10-16 20:31:59 +0530

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,7 @@
+soob=40*38
+print('sum of observations before is',soob)
+bef_mean=38
+print('mean before ',bef_mean)
+soob=soob+56-36
+print('after dececting sum of observation are', soob)
+print('mean is',soob/40)
