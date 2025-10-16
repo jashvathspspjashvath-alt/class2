fc8bc9a31662350c89ccad95c5d8f31ff61a3c95
jashvathspspjashvath-alt <jashvathspspjashvath@gmail.com>
2025-10-16 20:41:57 +0530

    Add files via upload

---
+++ ac22.py
@@ -0,0 +1,11 @@
+speed=10
+speed2=20
+speed3=30
+average_speed=(speed+speed2+speed3)/3
+print('avg speed', average_speed,'km/h')
+if speed< average_speed:
+    print('cyclist 1 is slower than avg')
+if speed2< average_speed:
+    print('cyclist 2 is slower tthan avg')
+if speed3< average_speed:
+    print('cyclist 3 is slower than avg')
