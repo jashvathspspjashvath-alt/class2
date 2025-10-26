a2edd5b472498a071aa8803cb10d98ad1db77946
jashvathspspjashvath-alt <jashvathspspjashvath@gmail.com>
2025-10-26 16:07:59 +0530

    Add files via upload

---
+++ jash3.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+if age >= 10:
+    if age <= 20:
+        print("Your age is between 10 and 20 years.")
+    else:
+        print("Your age is greater than 20 years.")
+else:
+    print("Your age is less than 10 years.")
