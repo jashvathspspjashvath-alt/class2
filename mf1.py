045b99689c5a40768f9ef5209c4efeb67f95e41d
jashvathspspjashvath-alt <jashvathspspjashvath@gmail.com>
2025-11-03 20:53:26 +0530

    Add files via upload

---
+++ mf1.py
@@ -0,0 +1,8 @@
+decimal = int(input("Enter a decimal number: "))
+binary = ""
+while decimal > 0:
+    remainder = decimal % 2
+    binary = str(remainder) + binary
+    decimal = decimal // 2
+
+print("Binary number:", binary)
