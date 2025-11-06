25f6e18641c8d76421f252e3e4fb4f57a55a6a13
jashvathspspjashvath-alt <jashvathspspjashvath@gmail.com>
2025-11-06 20:45:32 +0530

    Add files via upload

---
+++ soraw.py
@@ -0,0 +1,11 @@
+import turtle
+my_wn = turtle. Screen()
+my_wn.bgcolour('ligth blue')
+my_wn.title('turtle')
+my_wn = turtle.Turtle()
+size=0
+while True:
+     for i in range(4)
+     my_wn.left(98)
+     size = siz - 5
+    size = size +1
