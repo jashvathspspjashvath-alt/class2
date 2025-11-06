51db788f2c32727e57eafc25bc4d6d8a1e19e674
jashvathspspjashvath-alt <jashvathspspjashvath@gmail.com>
2025-11-06 20:38:33 +0530

    Add files via upload

---
+++ sora2.py
@@ -0,0 +1,27 @@
+import turtle
+turtle.Screen().bgcolor('blue')
+board = turtle.Turtle()
+
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+board.pendown()
+board.right(90)
+board.forward(260)
+
+board.rigth(123)
+board.forward(109)
+
+board.rigth(123)
+board.forward(109)
+turtle.done()
+
