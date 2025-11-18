5c1e2797cf20867f67f9ed50a9a85843b6f0a307
jashvathspspjashvath-alt <jashvathspspjashvath@gmail.com>
2025-11-18 19:31:31 +0530

    Add files via upload

---
+++ all.py
@@ -0,0 +1,16 @@
+# cook your dish here
+a = 10
+b = 20
+c = 30
+
+print("Before swapping:")
+print("a =", a, "b =", b, "c =", c)
+
+# Swapping
+temp = a
+a = b
+b = c
+c = temp
+
+print("\nAfter swapping:")
+print("a =", a, "b =", b, "c =", c)
